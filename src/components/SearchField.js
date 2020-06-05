c3a085c3fecb10b95716cd6cc3de34dd1efff62f
Chunhao Zheng <chz062009@gmail.com>
2020-06-05 19:13:03 -0400

    SearchField.js

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class SearchField extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      gifUrl: "",
+      gifUrl: [],
       searchTerm: null,
       API_Key: '&api_key=Y3gKxeNuyrgxRoDhBubClBALroKAG017',
+      data: [],
+      arrObj: false,
+      randomGif: null,
     }
   }
 
@@ -31,16 +34,19 @@ class SearchField extends Component {
         // data is an array containing objects
         const data = response.data;
 
+        this.setState({ arrObj: true});
+        this.setState({ data: data.data}); // transforming data into array
         console.log(data)
-
+        console.log(typeof data)
         // grabs a random integer from 0 to 9 so we can pick a random gif object from our 10 results
-        let randomGif = Math.floor(Math.random() * 10);
-        console.log(randomGif);
+        // let randomGif = Math.floor(Math.random() * 10);
+        // console.log(randomGif);
 
         // assign the variables we need
-        this.setState({ gifUrl: data.data[randomGif].images.original.url})
+        for(let i = 0; i < 10; i++){
+        this.setState({ gifUrl: data.data[i].images.original})
         console.log(this.state.gifUrl);
-
+        }
       })
       .catch((err) => console.log(err));
   }
@@ -49,32 +55,93 @@ class SearchField extends Component {
   handleTrendingSearch = () => {
     const trendingLink = 'http://api.giphy.com/v1/gifs/trending?' + this.state.API_Key;
     console.log(trendingLink);
+    axios
+      .get(trendingLink + "&limit=10", {
+          params: {  //_limit: 10
+          }
+        } 
+      )
+      .then((response) => {
+        // data is an array containing objects
+        const data = response.data;
+        this.setState({ arrObj: true});
+        this.setState({ data: data.data});
+        // console.log(data)
+        // console.log(typeof data)
+      
+      })
+      .catch((err) => console.log(err));
+  
   }
 
   handleRandomSearch = () => {
     const randomchLink = 'http://api.giphy.com/v1/gifs/random?' + this.state.API_Key;
-    console.log(randomchLink)
+    //  console.log(randomchLink)
+    axios
+    .get(randomchLink + "&limit=10", {
+        params: {  //_limit: 10
+        }
+      } 
+    )
+    .then((response) => {
+      // data is an array containing objects
+      const data = response.data;
+      this.setState({ arrObj: false});
+      this.setState({ randomGif: data.data.images.original.url});
+      // console.log(this.state.randomGif)
+      // console.log(typeof this.state.randomGif)
+    })
+    .catch((err) => console.log(err));
+
   }
 
   render() {
+    // console.log(typeof this.state.data.data)
+    // for(let i = 0; i<gifUrl.length; i++){
+    //   let imgList = this.state.gifUrl[i];
+    // }
+    let imgList;
+    if(this.state.arrObj){
+      imgList = this.state.data.map((x) =>(
+      <div className = 'url' key = {x}>
+        <img src = {x.images.original.url} alt = {this.state.searchTerm}/>
+      </div>
+     ))
+     }
+      else  { 
+        // let x = this.state.randomGif;
+        console.log(this.state.randomGif)
+        imgList = <img src = {this.state.randomGif} alt = "Image loading" />
+      }
+     
+
+
+    // let imgList = this.state.data.forEach((x) => {
+    //    return  <img src = {x.images.original.url} alt = {this.state.searchTerm} />
+    //    //(<div className = 'url' key = {x}>
+     
+    //   //  </div>)
+    // });
+
     return (
       <div>
-
+        <h1>Gifphy Assignment: </h1>
+        
         <p>
           <input type="text" placeholder="Regular Search" onChange={this.handleChange} />
           <button onClick={this.handleRegularSearch}>Search</button>
         </p>
 
         <p>
-          <input type="text" placeholder="Trending Search" onChange={this.handleChange} />
+          {/* <input type="text" placeholder="Trending Search" onChange={this.handleChange} /> */}
           <button onClick={this.handleTrendingSearch}>Trending Search</button>
         </p>
 
         <p>
-          <input type="text" placeholder="Random Search" onChange={this.handleChange} />
+          {/* <input type="text" placeholder="Random Search" onChange={this.handleChange} /> */}
           <button onClick={this.handleRandomSearch}>Random Search</button>
         </p>
-
+        <p>{imgList}</p> 
       </div>
 
     )
