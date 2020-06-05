9eadcd869ad43d710f24c001d0c1d8e79d8751a9
ShihaoDong1 <shihao.dong1@gmail.com>
2020-06-04 23:35:50 -0400

    update SearchField

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-// import Axios from 'axios';
-
+import axios from 'axios';
 
 class SearchField extends Component {
   constructor (props){
@@ -8,39 +7,75 @@ class SearchField extends Component {
     this.state = {
       imgLoad: null,
       searchTerm: null,
-      API_Key: '&api_key=Y3gKxeNuyrgxRoDhBubClBALroKAG017'
+      API_Key: '&api_key=Y3gKxeNuyrgxRoDhBubClBALroKAG017',
+      url1: "",
+      dataArray: [],
+
     }
   }
+
+  componentDidMount(){
+    
+    // console.log(this.state.url1);
+
+    axios
+      .get('http://api.giphy.com/v1/gifs/search?q='+this.state.searchTerm + this.state.API_Key, {   params: {
+        _limit: 10
+       }
+  })
+      .then((response) => {
+        const data = response.data;
+        console.log(data)
+        this.setState({dataArray:data});
+        console.log(data.data[0].type);
   
+      })
+      .catch((err) => console.log(err));
+
+  }
+
   handleRegularSearch = (event) => {
     // this.state.
     this.setState({searchTerm: event.target.value});
     const regularLink =  'http://api.giphy.com/v1/gifs/search?q='+this.state.searchTerm + this.state.API_Key;
+    this.setState({url1: regularLink})
     console.log(regularLink);
-  }
+
+    
+
+      // console.log(this.setState({city: data[0].City}))
+      // console.log(data);
+
+    }
+  
 
   handleTrendingSearch = () => {
     
     const trendingLink =  'http://api.giphy.com/v1/gifs/trending?'+ this.state.API_Key;
     console.log(trendingLink);
   }
+
   handleRandomSearch = () => {
     const randomchLink = 'http://api.giphy.com/v1/gifs/random?' + this.state.API_Key;
     console.log(randomchLink)
   }
 
+  handleChange = (event) => {
+    this.setState({searchTerm: event.target.value})
+  }
+
   render(){
-    
-    return <div>
+    return( <div>
       <p>
-      <input type = "text" placeholder = "Regular Search" onChange = {this.handleRegularSearch}/><button onClick = {this.handleRegularSearch}>Search</button>
+      <input type = 
+      "text" placeholder = "Regular Search" onChange = {this.handleChange}/><button onClick = {this.handleRegularSearch}>Search</button>
       </p>
       <p>
       <button onClick = {this.handleTrendingSearch}>Trending Search</button>
       </p>
       <button onClick = {this.handleRandomSearch}>Random Search</button>
     </div>
-    
+    )
   }
 }
 
